cb21fcfbb1f12d58e55aa04125e81aa4bf34db70
Rubén Garrido <rgarridovaldepenas@uoc.edu>
2022-05-21 14:58:35 +0200

    start moving the units + draw rectangle on screen to select multiple units

--- PEC4_RTS/Assets/Scripts/ActionManager.cs
+++ PEC4_RTS/Assets/Scripts/ActionManager.cs
@@ -6,12 +6,29 @@ public class ActionManager : MonoBehaviour
 {
 
     private GameObject selectedUnit = null;
+    private LineRenderer line;
+    private Vector2 startMousePosition, currentMousePosition;
+
+
+    private void Start()
+    {
+        line = GetComponent<LineRenderer>();
+        line.positionCount = 0;
+    }
 
     void Update()
     {        
         if (Input.GetMouseButtonDown(0))
-        {                        
+        {
+            startMousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            line.positionCount = 4;
+            line.SetPosition(0, new Vector2(startMousePosition.x, startMousePosition.y));
+            line.SetPosition(1, new Vector2(startMousePosition.x, startMousePosition.y));
+            line.SetPosition(2, new Vector2(startMousePosition.x, startMousePosition.y));
+            line.SetPosition(3, new Vector2(startMousePosition.x, startMousePosition.y));
+
             RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+
             if (hit.collider != null)
             {
                 if (hit.collider.gameObject.CompareTag("Soldier"))
@@ -37,9 +54,30 @@ public class ActionManager : MonoBehaviour
             if(selectedUnit != null)
             {
                 // TODO Obtain point
+                Vector3 newPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                newPosition.z = 0f;
+                Debug.Log("Click position " + newPosition);
+                selectedUnit.GetComponent<SoldierController>().Move(newPosition);
                 // TODO Move unit
             }
         }
 
+        if (Input.GetMouseButton(0))
+        {
+            currentMousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            line.SetPosition(0, new Vector2(startMousePosition.x, startMousePosition.y));
+            line.SetPosition(1, new Vector2(startMousePosition.x, currentMousePosition.y));
+            line.SetPosition(2, new Vector2(currentMousePosition.x, currentMousePosition.y));
+            line.SetPosition(3, new Vector2(currentMousePosition.x, startMousePosition.y));
+
+            var selectedArea = Mathf.Abs(
+                (startMousePosition.x - currentMousePosition.x) * 
+                (startMousePosition.y - currentMousePosition.y));
+        }
+
+        if(Input.GetMouseButtonUp(0))
+        {
+            line.positionCount = 0;
+        }
     }
 }
--- PEC4_RTS/Assets/Scripts/SoldierController.cs
+++ PEC4_RTS/Assets/Scripts/SoldierController.cs
@@ -9,6 +9,8 @@ public class SoldierController : MonoBehaviour
     public int healthPoints = 1;
     public GameObject selectedGameObject;
     private Animator animator;
+    private bool isFacingRight = true;
+    private Coroutine movingCoroutine;
 
     // Start is called before the first frame update
     void Start()
@@ -32,9 +34,24 @@ public class SoldierController : MonoBehaviour
         selectedGameObject.SetActive(false);
     }
 
-    public void Move(Transform newPosition)
+    public void Move(Vector3 newPosition)
     {
+        if(movingCoroutine != null)
+        {
+            StopCoroutine(movingCoroutine);
+        }        
+        movingCoroutine = StartCoroutine(Moving(newPosition));
+    }
 
+    IEnumerator Moving(Vector3 newPosition)
+    {
+        while(transform.position != newPosition)
+        {
+            animator.SetBool("Moving", true);            
+            transform.position = Vector3.Lerp(transform.position, newPosition,  0.1f* Time.deltaTime);
+            yield return new WaitForEndOfFrame();
+        }
+        animator.SetBool("Moving", false);
     }
 
     public void Attack()
@@ -47,5 +64,11 @@ public class SoldierController : MonoBehaviour
         animator.SetTrigger("Death");
     }
 
-
+    private void Flip()
+    {
+        isFacingRight = !isFacingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
 }
